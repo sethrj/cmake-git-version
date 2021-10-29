40a73b13add51deffe61078b1ab08ae25bd67dd8
Seth R Johnson <johnsonsr@ornl.gov>
2021-10-29 19:57:47 -0400

    Save CGV version to script after running

--- install.sh
+++ install.sh
@@ -17,6 +17,8 @@ SRC="$(dirname "$0")/CgvFindVersion.cmake"
 DST="$1/CgvFindVersion.cmake"
 cp "$SRC" "$DST"
 echo "-- Copied script to $DST" >&2
+VERSION="$(cmake -D ONLY=VERSION_STRING -P $SRC  3>&2 2>&1 1>&3)"
+echo "\n# cmake-git-version ${VERSION}" >> "${DST}"
 
 GITATTR="$1/.gitattributes"
 if [ ! -e "$GITATTR" ] \
